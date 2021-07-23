858ee3261529a4f70221ddd0b32dd2647bb2e59d
ViRybalkin <kinjeks@gmail.com>
2021-07-23 11:54:59 +0300

    Add css,html,  login and show data

---
+++ script.js
@@ -0,0 +1,139 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const creatMovements = movements => {
+  containerMovements.innerHTML = '';
+  movements.forEach((val, ind) => {
+    const type = val < 0 ? 'withdrawal' : 'deposit';
+    const html = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${
+      ind + 1
+    } deposit</div>
+    <div class="movements__date"></div>
+    <div class="movements__value">${val}€</div>
+  </div>
+  
+  `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const createUserName = accs => {
+  accs.forEach(acc => {
+    acc.userName = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUserName(accounts);
+
+const displayTotal = mov => {
+  const total = mov.reduce((acc, curVal) => {
+    return (acc += curVal);
+  });
+  labelBalance.textContent = `${total}€`;
+};
+
+const displaySummary = account => {
+  const sumIn = account.movements
+    .filter(item => item > 0)
+    .reduce((acc, curVal) => acc + curVal, 0);
+  labelSumIn.textContent = `${sumIn}€`;
+
+  const sumOut = account.movements
+    .filter(item => item < 0)
+    .reduce((acc, curVal) => acc + curVal, 0);
+  labelSumOut.textContent = `${Math.abs(sumOut)}€`;
+
+  const sumInterest = account.movements
+    .filter(item => item > 0)
+    .map(deposit => (deposit * account.interestRate) / 100)
+    .filter(dep => dep >= 0)
+    .reduce((acc, curVal) => acc + curVal, 0);
+  labelSumInterest.textContent = `${sumInterest}€`;
+};
+
+let currentAccount;
+btnLogin.addEventListener('click', e => {
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.userName === inputLoginUsername.value
+  );
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    labelWelcome.textContent = `Welcome back ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 1;
+
+    inputLoginPin.value = inputLoginUsername.value = '';
+
+    creatMovements(currentAccount.movements);
+    displayTotal(currentAccount.movements);
+    displaySummary(currentAccount);
+  }
+});
